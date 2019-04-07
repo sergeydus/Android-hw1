d89dad198e46e22843b96f7898bd34ca5ea71bfb
roiwei <48522938+roiwei@users.noreply.github.com>
2019-04-07 18:14:41 +0300

    #3
    #3

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        Button clicker= findViewById(R.id.clicker);
-//        clicker.setText("CLICK ME TO START TTHEGAEM OR ELSE");
+//        clickersetText("CLICK ME TO START TTHEGAEM OR ELSE");
 //        TextView results= findViewById(R.id.results);
 //        results.setText("YOU'VE CLICKED ME "+Timesclicked+" TIMES!");
 //
